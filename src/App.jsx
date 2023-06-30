3dc1785ec80ea62b60c4566db197f70da92cfde2
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2023-06-30 18:54:34 -0300

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/LandingPage";
-import Home from "./components/Home";
-// import Create from "./components/Create.jsx";
-import Create from "./components/Create";
-import Detail from "./components/Detail";
-import NotFound from "./components/404";
-
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from "./components/LandingPage"
+import Home from "./components/Home"
+import Create from "./components/Create"
+import Detail from "./components/Detail"
+import NotFound from "./components/NotFound"
+import axios from "axios"
+axios.defaults.baseURL = "https://videogames-back-production-4a4a.up.railway.app/"
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<Home />} />
           <Route path="/create" element={<Create />} />
-          <Route path="/pokemon/:id" element={<Detail />} />
-          <Route path="/*" element={<NotFound />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
